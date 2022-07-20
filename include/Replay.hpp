36a670fb8c0b6c1ace95ce0b65b6ada6cf962e2a
ej6QrPxsn <yms@kfy.biglobe.ne.jp>
2022-07-20 14:43:50 +0200

    サンプル生成を別スレッドにし、訓練時の速度90steps/s以上で安定。

--- include/Replay.hpp
+++ include/Replay.hpp
@@ -1,73 +1,10 @@
 #ifndef REPLAY_HPP
 #define REPLAY_HPP
 
-#include "Request.hpp"
-#include "SumTree.hpp"
-#include "Utils.hpp"
+#include "ReplayBuffer.hpp"
 #include <deque>
 #include <future>
-#include <memory>
 #include <mutex>
-#include <random>
-class ReplayBuffer {
-public:
-  ReplayBuffer(int capacity) : tree(SumTree(capacity)), count(0) {}
-
-  int get_count() { return count; }
-
-  void update(int idx, float p) { tree.update(idx, p); }
-
-  void add(float p, ReplayData sample) {
-    tree.add(p, sample);
-    count += 1;
-    if (count < REPLAY_BUFFER_MIN_SIZE) {
-      if (count % REPLAY_BUFFER_ADD_PRINT_SIZE == 0) {
-        std::cout << "Waiting for the replay buffer to fill up. "
-                  << "It currently has " << count;
-        std::cout << " elements, waiting for at least "
-                  << REPLAY_BUFFER_MIN_SIZE << " elements" << std::endl;
-      }
-    } else if (count == REPLAY_BUFFER_MIN_SIZE) {
-      std::cout << "Replay buffer filled up. "
-                << "It currently has " << count << " elements.";
-      std::cout << " Start training." << std::endl;
-    }
-  }
-
-  std::tuple<std::vector<int>, std::vector<ReplayData>> sample(int n) {
-    std::vector<int> idx_list;
-    std::vector<ReplayData> data_list;
-
-    auto segment = tree.total() / n;
-
-    for (auto i = 0; i < n; i++) {
-      auto a = segment * i;
-      auto b = segment * (i + 1);
-
-      std::random_device rd;
-      std::default_random_engine eng(rd());
-      std::uniform_int_distribution<int> distr(a, b);
-
-      auto s = distr(eng);
-      if (s == 0) {
-        s = 1;
-      }
-      auto ret = tree.get(s);
-      auto index = std::get<0>(ret);
-      auto data = std::get<2>(ret);
-
-      //(idx, p, data)
-      idx_list.emplace_back(index);
-      data_list.emplace_back(data);
-    }
-
-    return {idx_list, data_list};
-  }
-
-private:
-  SumTree tree;
-  int count;
-};
 
 class Replay {
 public:
@@ -78,6 +15,8 @@ public:
 
     addThread =
         std::thread(&Replay::addLoop, this, std::move(bufferNotification));
+    sampleThread =
+        std::thread(&Replay::sampleLoop, this);
   }
 
   void updatePriorities(std::vector<int> indexes, torch::Tensor priorities) {
@@ -89,14 +28,14 @@ public:
   void putReplayQueue(torch::Tensor priorities, std::vector<ReplayData> data) {
     if (replayQueue.size() < MAX_REPLAY_QUEUE_SIZE) {
       replayQueue.emplace_back(std::move(std::make_tuple(priorities, data)));
-      condVar.notify_one();
+      replayCond.notify_one();
     }
   }
 
   void addReplay() {
     {
       std::unique_lock<std::mutex> lck(replayMtx);
-      condVar.wait(lck, [&] { return !replayQueue.empty(); });
+      replayCond.wait(lck, [&] { return !replayQueue.empty(); });
     }
 
     auto queueData = std::move(replayQueue.front());
@@ -115,6 +54,8 @@ public:
       addReplay();
     }
 
+    getSample();
+
     bufferNotification.set_value();
 
     while (1) {
@@ -122,21 +63,47 @@ public:
     }
   }
 
+  void getSample() {
+      auto sample = buffer.sample(BATCH_SIZE);
+      auto indexes = std::get<0>(sample);
+      auto data = dataConverter.toBatchedTrainData(std::get<1>(sample));
+      sampleData = std::make_tuple(indexes, data);
+  }
+
+  void sampleLoop() {
+    while (1) {
+      {
+        std::unique_lock<std::mutex> lck(sampleMtx);
+        sampleCond.wait(lck, [&] { return isSample; });
+      }
+      getSample();
+      isSample = false;
+    }
+  }
+
   auto sample() {
     waitForNotification.wait();
-    auto sample = buffer.sample(BATCH_SIZE);
-    auto indexes = std::get<0>(sample);
-    auto data = dataConverter.toBatchedTrainData(std::get<1>(sample));
-    return std::make_tuple(indexes, data);
+
+    auto data = std::move(sampleData);
+
+    isSample = true;
+    sampleCond.notify_one(); 
+
+    return data;
   }
 
   std::future<void> waitForNotification;
   std::thread addThread;
+  std::thread sampleThread;
   DataConverter dataConverter;
   ReplayBuffer buffer;
   std::deque<std::tuple<torch::Tensor, std::vector<ReplayData>>> replayQueue;
   std::mutex replayMtx;
-  std::condition_variable condVar;
+  std::condition_variable replayCond;
+  std::mutex sampleMtx;
+  std::condition_variable sampleCond;
+  bool isSample = false;
+  std::tuple<std::vector<int>, TrainData> sampleData;
 };
 
 #endif // REPLAY_HPP
---
+++ include/ReplayBuffer.hpp
@@ -0,0 +1,70 @@
+#ifndef REPLAY_BUFFER_HPP
+#define REPLAY_BUFFER_HPP
+
+#include "Request.hpp"
+#include "SumTree.hpp"
+#include "Utils.hpp"
+#include <memory>
+#include <random>
+
+class ReplayBuffer {
+public:
+  ReplayBuffer(int capacity) : tree(SumTree(capacity)), count(0) {}
+
+  int get_count() { return count; }
+
+  void update(int idx, float p) { tree.update(idx, p); }
+
+  void add(float p, ReplayData sample) {
+    tree.add(p, sample);
+    count += 1;
+    if (count < REPLAY_BUFFER_MIN_SIZE) {
+      if (count % REPLAY_BUFFER_ADD_PRINT_SIZE == 0) {
+        std::cout << "Waiting for the replay buffer to fill up. "
+                  << "It currently has " << count;
+        std::cout << " elements, waiting for at least "
+                  << REPLAY_BUFFER_MIN_SIZE << " elements" << std::endl;
+      }
+    } else if (count == REPLAY_BUFFER_MIN_SIZE) {
+      std::cout << "Replay buffer filled up. "
+                << "It currently has " << count << " elements.";
+      std::cout << " Start training." << std::endl;
+    }
+  }
+
+  std::tuple<std::vector<int>, std::vector<ReplayData>> sample(int n) {
+    std::vector<int> idx_list;
+    std::vector<ReplayData> data_list;
+
+    auto segment = tree.total() / n;
+
+    for (auto i = 0; i < n; i++) {
+      auto a = segment * i;
+      auto b = segment * (i + 1);
+
+      std::random_device rd;
+      std::default_random_engine eng(rd());
+      std::uniform_int_distribution<int> distr(a, b);
+
+      auto s = distr(eng);
+      if (s == 0) {
+        s = 1;
+      }
+      auto ret = tree.get(s);
+      auto index = std::get<0>(ret);
+      auto data = std::get<2>(ret);
+
+      //(idx, p, data)
+      idx_list.emplace_back(index);
+      data_list.emplace_back(data);
+    }
+
+    return {idx_list, data_list};
+  }
+
+private:
+  SumTree tree;
+  int count;
+};
+
+#endif // REPLAY_BUFFER_HPP
